7276d17b620a1e9a5d81dcfe98897ae56b26df66
SanchitGarg08 <e1633679@u.nus.edu>
2026-08-17 13:26:17 +0800

    Level 0 Greet/Bye added

--- src/main/java/Floppy.java
+++ src/main/java/Floppy.java
@@ -2,12 +2,20 @@
 public class Floppy {
 
     public static void main(String[] args) {
+        String horizontalLine = "____________________________________________________________";
         String banner = " _____ _                      \n"
                 + "|  ___| | ___  _ __  _ __  _   _\n"
                 + "| |_  | |/ _ \\| '_ \\| '_ \\| | | |\n"
                 + "|  _| | | (_) | |_) | |_) | |_| |\n"
                 + "|_|   |_|\\___/| .__/| .__/ \\__, |\n"
-                + "              |_|   |_|    |___/\n";
+                + "              |_|   |_|    |___/";
+
+        System.out.println(horizontalLine);
         System.out.println(banner);
+        System.out.println("Hello! I'm Floppy.");
+        System.out.println("What can I do for you?");
+        System.out.println(horizontalLine);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(horizontalLine);
     }
 }
